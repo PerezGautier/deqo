7ea0ad85bd6d8adddd4301ccdeb57b75b49a8f35
PerezGautier <gautier.perez@etud.univ-jfc.fr>
2020-10-16 10:47:24 +0200

    add gitignore

---
+++ src/main/java/deqo/gper/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.gper.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
