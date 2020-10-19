acf24cc5f3517bec9ffd0f5bec92410b741a176c
PerezGautier <gautier.perez@etud.univ-jfc.fr>
2020-10-19 14:49:17 +0200

    #2 add more gateaux

--- src/main/java/deqo/gper/mysimplestack/App.java
+++ src/main/java/deqo/gper/mysimplestack/App.java
@@ -8,6 +8,8 @@ public class App
         StackOfGateau stackOfGateau = new StackOfGateau(100);
         stackOfGateau.push(new Gateau("ChocolateCake","Chocolate",1));
         stackOfGateau.push(new Gateau("Cookies","Chocolate",1));
+        stackOfGateau.push(new Gateau("MousseAuChocolat","Chocolate",1));
+        stackOfGateau.push(new Gateau("Brownies","Chocolate",1));
 
         System.out.println(stackOfGateau.toString());
 
